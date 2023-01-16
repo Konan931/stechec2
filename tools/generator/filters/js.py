8b90818fd525910ca5f229b9d6d1ddc79c92632a
LeSeulArtichaut <leseulartichaut@gmail.com>
2023-01-16 10:45:19 +0100

    Add copyright notices
    As asked by @seirl.

--- tools/generator/filters/js.py
+++ tools/generator/filters/js.py
@@ -1,3 +1,7 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2022 Association Prologin <association@prologin.org>
+# Copyright (c) 2022 Léo Lanteri Thauvin
+
 from typing import List
 
 from . import register_filter
