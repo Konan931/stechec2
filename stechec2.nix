ff53c9f1050cf30f38908ad94a7582a41e05a2d1
lportemo <5241257-lportemo@users.noreply.gitlab.com>
2026-04-14 22:19:51 +0200

    nix: refacto mkStechec2Game

--- stechec2.nix
+++ stechec2.nix
@@ -5,8 +5,8 @@
     doCheck ? true,
     cmakeFlags ? [],
     buildPlayerEnvironment ? true,
-  }: let
-    game = pkgs.stdenv.mkDerivation {
+  }:
+    pkgs.stdenv.mkDerivation {
       inherit name src doCheck;
       cmakeFlags =
         (
@@ -26,24 +26,9 @@
         pkgs.gtest
         pkgs.stechec2
       ];
+      postInstall = pkgs.lib.optionalString buildPlayerEnvironment ''
+        ${pkgs.stechec2-generator}/bin/stechec2-generator player \
+            $out/share/stechec2/${name}/${name}.yml $out/share/stechec2/${name}/player
+      '';
     };
-    playerEnvironment = pkgs.runCommand "${name}-player-environment" {} ''
-      mkdir -p $out/share/stechec2/${name}/player
-      ${pkgs.stechec2-generator}/bin/stechec2-generator \
-        player \
-        ${game}/share/stechec2/${name}/${name}.yml \
-        $out/share/stechec2/${name}/player
-    '';
-  in
-    # FIXME : better permissions handling
-    pkgs.runCommand "${name}" {} ''
-      mkdir -p $out
-      cp --no-preserve=mode -r ${game}/* $out/
-      chmod +x $out/lib/lib${name}.so
-      ${
-        if buildPlayerEnvironment
-        then "cp --no-preserve=mode -r ${playerEnvironment}/* $out/"
-        else ""
-      }
-    '';
 }
